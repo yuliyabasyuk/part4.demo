4619d29c6a30a40ccfb625ad37034d216b5ac500
yuliyabasyuk <yuliya.basyuk.92@gmail.com>
2018-04-24 17:31:48 +0300

    2nd note as team1

--- part4/demo/part4demo.java
+++ part4/demo/part4demo.java
@@ -5,6 +5,7 @@ public class part4demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("tutorial4");
+		System.out.println("tutorial4. Second note");
 
 	}
 
